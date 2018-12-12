a75ecd423dc7102c6b00e00c8c9ea3b074e51e19
Mangon <gaoxiang.like@163.com>
2018-12-12 14:32:50 +0800

    Site updated: 2018-12-12 14:32:50

--- js/common.js
+++ js/common.js
@@ -1,10 +1,3 @@
-// 禁止右键菜单
-document.oncontextmenu = function () { return false; };
-// 禁止文字选择
-document.onselectstart = function () { return false; };
-// 禁止复制
-document.oncopy = function () { return false; };
-// 禁止剪切
-document.oncut = function () { return false; };
-// 禁止粘贴
-document.onpaste = function () { return false; };
+// build time:Wed Dec 12 2018 14:32:45 GMT+0800 (CST)
+document.oncontextmenu=function(){return false};document.onselectstart=function(){return false};document.oncopy=function(){return false};document.oncut=function(){return false};document.onpaste=function(){return false};
+//rebuild by neat 
