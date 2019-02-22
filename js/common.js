571de442696dee39b6b3102aac32a1ed9673dcbc
Mangon <gaoxiang.like@163.com>
2019-02-22 09:54:18 +0800

    Site updated: 2019-02-22 09:54:17

--- js/common.js
+++ js/common.js
@@ -1,3 +1,3 @@
-// build time:Thu Feb 21 2019 18:53:41 GMT+0800 (CST)
+// build time:Fri Feb 22 2019 09:54:09 GMT+0800 (CST)
 document.oncontextmenu=function(){return false};document.onselectstart=function(){return false};document.oncopy=function(){return false};document.oncut=function(){return false};document.onpaste=function(){return false};
 //rebuild by neat 
