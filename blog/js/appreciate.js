6fb867ac2bb1633721bc99df34c68d344807614e
Mangon <gaoxiang.like@163.com>
2021-01-19 13:57:23 +0800

    Site updated: 2021-01-19 13:57:22

--- blog/js/appreciate.js
+++ blog/js/appreciate.js
@@ -1,3 +1,3 @@
-// build time:Mon Jan 18 2021 20:03:14 GMT+0800 (GMT+08:00)
+// build time:Tue Jan 19 2021 13:57:20 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
--- blog/js/common.js
+++ blog/js/common.js
@@ -1,3 +1,3 @@
-// build time:Mon Jan 18 2021 20:03:14 GMT+0800 (GMT+08:00)
+// build time:Tue Jan 19 2021 13:57:20 GMT+0800 (GMT+08:00)
 (function(n,t,e){function o(){t.oncontextmenu=function(){return false};t.onselectstart=function(){return false};t.oncopy=function(){return false};t.oncut=function(){return false};t.onpaste=function(){return false};t.onbeforeprint=function(){return false}}function r(){var o=t.createElement("canvas");var r=o.getContext("2d");r.font="20px Arial";r.textAlign="center";r.textBaseline="bottom";r.fillStyle="#F3F4F6";r.rotate(-20*Math.PI/180);r.fillText(n.location.host,150,100);var c=o.toDataURL();var u='url("'+c+'")';var f=e("#content");f.css("background",u),a(f,"background",500)}function a(n,t,e){var o=n.css(t);setInterval(function(){var e=n.css(t);if(o!=e){n.css(t,o)}},e)}o();r()})(window,document,jQuery);
 //rebuild by neat 
