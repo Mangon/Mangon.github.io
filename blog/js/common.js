a03f1451ef1caf520f51d76ef0ab4a26daf6832f
Mangon <gaoxiang.like@163.com>
2021-03-16 14:00:48 +0800

    Site updated: 2021-03-16 14:00:47

--- blog/js/common.js
+++ blog/js/common.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 09 2021 15:53:22 GMT+0800 (中国标准时间)
+// build time:Tue Mar 16 2021 14:00:45 GMT+0800 (中国标准时间)
 (function(n,t,e){function o(){t.oncontextmenu=function(){return false};t.onselectstart=function(){return false};t.oncopy=function(){return false};t.oncut=function(){return false};t.onpaste=function(){return false};t.onbeforeprint=function(){return false}}function r(){var o=t.createElement("canvas");var r=o.getContext("2d");r.font="20px Arial";r.textAlign="center";r.textBaseline="bottom";r.fillStyle="#F3F4F6";r.rotate(-20*Math.PI/180);r.fillText(n.location.host,150,100);var c=o.toDataURL();var u='url("'+c+'")';var f=e("#content");f.css("background",u),a(f,"background",500)}function a(n,t,e){var o=n.css(t);setInterval(function(){var e=n.css(t);if(o!=e){n.css(t,o)}},e)}o();r()})(window,document,jQuery);
 //rebuild by neat 
