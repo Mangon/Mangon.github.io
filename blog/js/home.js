b3160739978409f61842e6009388ea0a095fa7e7
Mangon <gaoxiang.like@163.com>
2021-04-27 01:31:53 +0800

    Site updated: 2021-04-27 01:31:53

--- blog/js/home.js
+++ blog/js/home.js
@@ -1,3 +1,3 @@
-// build time:Sun Apr 25 2021 01:41:57 GMT+0800 (中国标准时间)
+// build time:Tue Apr 27 2021 01:31:51 GMT+0800 (中国标准时间)
 $(document).ready(function(){var r=2;var a=$(".original").html();var e=$("<div/>").insertBefore(".original");var i=$("<div/>").insertBefore(".original");for(var o=0;o<r;o++){$("<div/>").addClass("realistic blur blur-top").html(a).appendTo(e);$("<div/>").addClass("realistic blur blur-bottom").html(a).appendTo(i)}var d=$(".home")[0];if(d){var l=$(".footer",d.parentNode.parentNode);l.addClass("hide")}});
 //rebuild by neat 
